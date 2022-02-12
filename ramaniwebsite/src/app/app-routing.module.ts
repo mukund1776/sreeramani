8855821814929bfd39849767fb95a227b6d482c4
mukund <mukund1776@gmail.com>
2022-02-12 14:01:59 +0530

    .

--- ramaniwebsite/src/app/app-routing.module.ts
+++ ramaniwebsite/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ImagesComponent} from './images/images.component';
+import {ImageComponent} from './image/image.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ImagesComponent},
+  {path: 'image', component: ImageComponent},
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ramaniwebsite/src/app/app.module.ts
+++ ramaniwebsite/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './home/home.component';
+import { ImageComponent } from './image/image.component';
+import { ImagesComponent } from './images/images.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    HomeComponent,
+    ImageComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
